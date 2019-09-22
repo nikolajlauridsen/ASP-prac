a035ed0a56b2bce7292bfb09bcd842dd00f0f849
Molex <simplesleeptimer@gmail.com>
2019-09-22 17:28:31 +0200

    Do Add a new field

--- MvcMovie/Migrations/20190922152623_CreatePliz.Designer.cs
+++ MvcMovie/Migrations/20190922152623_CreatePliz.Designer.cs
@@ -9,8 +9,8 @@ using MvcMovie.Models;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20190922144355_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20190922152623_CreatePliz")]
+    partial class CreatePliz
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -25,7 +25,10 @@ namespace MvcMovie.Migrations
 
                     b.Property<string>("Genre");
 
-                    b.Property<decimal>("Price");
+                    b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18,2)");
+
+                    b.Property<string>("Rating");
 
                     b.Property<DateTime>("ReleaseDate");
 
