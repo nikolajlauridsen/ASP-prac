a0397798f98a1f06a38f93e0dacf72c74fa90da4
Molex <simplesleeptimer@gmail.com>
2019-12-16 12:30:40 +0100

    Add projects

---
+++ GaveBodAPI/GaveBodAPI/Controllers/Gifts.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+
+using GaveBodAPI.Models;
+
+namespace GaveBodAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Gifts : ControllerBase
+    {
+        private static List<Gift> gifts = new List<Gift>();
+        private static object GiftLock = new object();
+
+        public Gifts()
+        {
+            if(gifts.Count == 0)
+            {
+                gifts.Add(new Gift
+                {
+                    GiftNumber = 1,
+                    CreationDate = DateTime.Now,
+                    Title = "Xbox One",
+                    Description = "En spillekonsol",
+                    BoyGift = true,
+                    GirlGift = true
+                });
+
+                gifts.Add(new Gift
+                {
+                    GiftNumber = 2,
+                    CreationDate = DateTime.Now,
+                    Title = "PS4 Pro",
+                    Description = "En spillekonsol",
+                    BoyGift = true,
+                    GirlGift = true
+                });
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Gift> Get()
+        {
+            return gifts;
+        }
+
+        [HttpGet("{GiftNumber}")]
+        public Gift Get(int GiftNumber)
+        {
+            Gift gift = gifts.Where(g => g.GiftNumber == GiftNumber).First();
+            return gift;
+        }
+
+        [HttpPost]
+        public ActionResult PostGift([Bind("Title", "Description", "BoyGift", "GirlGift")] Gift gift)
+        {
+            lock (GiftLock)
+            {
+                gift.GiftNumber = gifts.Last().GiftNumber + 1;
+                gift.CreationDate = DateTime.Now;
+                gifts.Add(gift);
+            }
+            
+            return CreatedAtAction("Get", new { GiftNumber = gift.GiftNumber }, gift);
+        }
+    }
+}
---
+++ GaveBodAPI/GaveBodAPI/Models/Gift.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GaveBodAPI.Models
+{
+    public class Gift
+    {
+        public int GiftNumber { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public bool BoyGift { get; set; }
+
+        public bool GirlGift { get; set; }
+    }
+}
