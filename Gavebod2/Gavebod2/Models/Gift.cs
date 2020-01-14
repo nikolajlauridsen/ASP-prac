94d4a02bd9bbd7af18161740b8739ac34edc37ae
Mole <nikolajlauridsen@protonmail.ch>
2020-01-14 16:16:17 +0100

    Forgot a thing

--- Gavebod2/Gavebod2/Models/Gift.cs
+++ Gavebod2/Gavebod2/Models/Gift.cs
@@ -16,7 +16,7 @@ namespace Gavebod2.Models
         public string Title { get; set; }
 
         [Required]
-        [StringLength(1000, MinimumLength = 3, ErrorMessage = "String must be between 3 and 100 characters long")]
+        [StringLength(1000, MinimumLength = 3, ErrorMessage = "String must be between 3 and 1000 characters long")]
         public string Description { get; set; }
 
         [Display(Name = "Created")]
